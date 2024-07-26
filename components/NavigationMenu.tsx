b4720b70bf2336b60ba08033e0a2ac6d432999e2
Yemelyanov <nikolaef.post@gmail.com>
2024-07-26 09:33:27 +0300

    feat: start

---
+++ components/NavigationMenu.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Col } from 'react-bootstrap';
+
+const NavigationMenu = () => {
+
+    return (
+        <Col className='border' md={2}>
+            NavigationMenu
+        </Col>
+    );
+};
+
+export default NavigationMenu;
---
+++ components/TopMenu.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Col } from 'react-bootstrap';
+const TopMenu = () => {
+    return (
+        <Col className="border">
+            TopMenu
+        </Col>
+    );
+};
+
+export default TopMenu;
