5e0b182b3e503bf46f90a43662dd51c62f87d7a5
Yemelyanov <nikolaef.post@gmail.com>
2024-08-04 17:03:50 +0300

    feat: use lazy and Suspense

--- app/groups/page.tsx
+++ app/groups/page.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const Page = () => {
+
+
     return (
         <main className='h-100 d-flex justify-content-center align-items-center flex-column'>
             <h2>Группы</h2>
